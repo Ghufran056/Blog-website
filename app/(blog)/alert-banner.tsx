a5c60b6f562034aefceeb9abbc4573f5bff9af95
Ghufran056 <shahghufran40@gmail.com>
2025-01-03 19:51:47 +0500

    updated files

--- app/(blog)/alert-banner.tsx
+++ app/(blog)/alert-banner.tsx
@@ -1,23 +1,22 @@
-// "use client";
-
-// import { useRouter } from "next/navigation";
-// import { useSyncExternalStore, useTransition } from "react";
-
-// import { disableDraftMode } from "./actions";
-
-// const emptySubscribe = () => () => {};
+import Link from "next/link";
 
 export default function AlertBanner() {
-
-
   return (
     <div
-      className="
-       fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur"
+      className="fixed top-0 left-0 z-50 w-full bg-white/95 text-black border-b backdrop-blur flex items-center justify-between px-4 h-10 shadow-md"
     >
-      <div className="py-2 text-center text-sm">
-       BLOGS
+      {/* Logo Section */}
+      <div className="flex items-center space-x-2">
+       
+        <Link href={"/"}><h1 className="text-lg font-bold">BLOGSITE</h1></Link>
       </div>
+
+      {/* Navigation Links */}
+      <nav className="flex space-x-6 text-sm font-medium">
+        <Link href="/" className="hover:text-blue-500">Home</Link>
+        <Link href="/About" className="hover:text-blue-500">About</Link>
+        <Link href="/Contact" className="hover:text-blue-500">Contact</Link>
+      </nav>
     </div>
   );
 }
